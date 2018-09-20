74010b87370384b3cbe09ddc82c6fdd64332ee90
Joris.Craessaerts <craessaerts.joris@outlook.com>
2018-09-20 22:00:24 +0200

    Changed Slot Syle Behavior

--- Advanced Component Usage/src/App.vue
+++ Advanced Component Usage/src/App.vue
@@ -2,18 +2,28 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+               <app-quote>
+                   <h2>{{ quoteTitle }}</h2>
+                   <p>A Wonderful Quote!</p>
+               </app-quote>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-
-    export default {
-        
-    }
+import Quote from "./components/Quote.vue";
+export default {
+  data: function() {
+    return {
+      quoteTitle: "The Quote"
+    };
+  },
+  components: {
+    appQuote: Quote
+  }
+};
 </script>
 
-<style>
+<style scoped>
 </style>
---
+++ Advanced Component Usage/src/components/Quote.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <slot></slot>
+    <h2>qdfqdfqdsfqsdfqdsfqd</h2>
+</div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style scoped>
+div {
+  border: 1px solid #ccc;
+  box-shadow: 1px 1px 2px black;
+  padding: 30px;
+  margin: 30px auto;
+  text-align: center;
+}
+
+h2 {
+    color: red;
+}
+
+</style>
+
+
+
